780d8966f51d5d71c101b6924e0148686687c695
satrong <satrong@163.com>
2018-02-28 13:58:24 +0800

    change first param, allow dom collection or array

--- src/index.js
+++ src/index.js
@@ -7,24 +7,27 @@ import IPA from './ipa.js';
 
 /**
  * 入口函数
- * @param {Object} fileInputDom fileinput元素
+ * @param {DomObject|DomObjectArray} doms fileinput元素
  * @param {Function} callback 回调函数
  * @param {String} workerScriptsPath 设置引用 zip/inflate.js和z-worker.js的路径
  */
-const APK_IPA_READER = function (fileInputDom, callback, workerScriptsPath) {
+const APK_IPA_READER = function (doms, callback, workerScriptsPath) {
     zip.workerScriptsPath = workerScriptsPath;
-    fileInputDom.addEventListener('change', function () {
-        var file = fileInputDom.files[0];
-        model.getEntries(file, function (entries) {
-            if (/\.apk$/i.test(file.name)) {
-                APK(entries, callback/* (err, fileinfo) */);
-            } else if (/\.ipa$/i.test(file.name)) {
-                IPA(entries, callback/* (err, fileinfo) */);
-            } else {
-                callback('只支持apk和ipa文件格式');
-            }
-        });
-    }, false);
+    doms = /HTMLCollection|Array/i.test(Object.prototype.toString.call(doms)) ? doms : [doms];
+    Array.prototype.forEach.call(doms, dom => {
+        dom.addEventListener('change', function () {
+            var file = dom.files[0];
+            model.getEntries(file, function (entries) {
+                if (/\.apk$/i.test(file.name)) {
+                    APK(entries, callback/* (err, fileinfo) */);
+                } else if (/\.ipa$/i.test(file.name)) {
+                    IPA(entries, callback/* (err, fileinfo) */);
+                } else {
+                    callback('只支持apk和ipa文件格式');
+                }
+            });
+        }, false);
+    });
 }
 
 export default APK_IPA_READER;
